5443783fb6e8d6a84e08bce27ea1640f332a7ec0
Bolkof <142789552+Bolkof@users.noreply.github.com>
2023-09-11 10:52:05 -0500

    Add files via upload
    Primer commit desde la página de github

---
+++ colores_ascii.dart
@@ -0,0 +1,23 @@
+rojo = "\033[91m"
+cyan = "\033[96m"
+magenta = "\033[95m"
+blanco = "\033[97m"
+verde = "\033[92m"
+azul = "\033[94m"
+amarillo = "\033[93m"
+negro = "\033[90m"
+
+reset_color = "\033[0m"
+subrayado = "\033[4m"
+parpadeante = "\033[5m"
+invertido = "\033[7m"
+tachado = "\033[9m"
+
+
+amarillo_brillante = "\033[93;1m"
+rojo_brillante = "\033[91;1m"
+verde_brillante = "\033[92;1m"
+azul_brillante = "\033[94;1m"
+magenta_brillante = "\033[95;1m"
+cyan_brillante = "\033[96;1m"
+blanco_brillante = "\033[97;1m"
